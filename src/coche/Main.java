b28af72570903a88c318c959827dd7cfa19f0b0c
Usuario <Usuario@DESKTOP-S5FHSBQ>
2021-04-15 13:49:40 +0200

    v1 - Comenzamos a modificar v2 - Encapsulamiento de campos y borrado de lineas vacias v3 - Modificación de la Clase Main

--- src/coche/Main.java
+++ src/coche/Main.java
@@ -12,15 +12,14 @@ public class Main {
         int stockActual;
         
         miCoche = new Coche("Opel",12000,500);
-        try 
-        {
-            System.out.println("Venta de Coches");
-            miCoche.vender(300);
-        } catch (Exception e)
-        {
-            System.out.print("Fallo al vender");
-        }
+        operativaVenta(miCoche);
         
+        operativaCompra(miCoche);
+        stockActual = miCoche.obtenerStock();
+        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    public static void operativaCompra(Coche miCoche) {
         try
         {
             System.out.println("Compra de Coches");
@@ -29,8 +28,17 @@ public class Main {
         {
             System.out.print("Fallo al ingresar");
         }
-        stockActual = miCoche.obtenerStock();
-        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    public static void operativaVenta(Coche miCoche) {
+        try
+        {
+            System.out.println("Venta de Coches");
+            miCoche.vender(300);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al vender");
+        }
     }
 
 }
